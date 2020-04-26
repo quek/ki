6a96ae31624a2c6dd12aa97b8f27e4be710228c9
Tahara Yoshinori <read.eval.print@gmail.com>
2020-04-26 20:37:06 +0900

    form の共通化

--- client/src/component/admin/posts/edit.rs
+++ client/src/component/admin/posts/edit.rs
@@ -1,25 +1,22 @@
 use crate::common::dto::{Id, Post};
-use crate::common::form::{PostErrors, PostForm};
-use crate::common::types::PostStatus;
-use crate::component::error;
+use crate::common::form::PostForm;
+use crate::component::admin::posts::form;
 use crate::fetch;
 use crate::routes::{AdminRoute, AppRoute};
 use crate::utils;
-use web_sys::Event;
 use yew::{html, Component, ComponentLink, Html, Properties, ShouldRender};
 
 pub struct Model {
     link: ComponentLink<Self>,
     props: Props,
     post: Option<Post>,
-    errors: PostErrors,
     #[allow(dead_code)]
     fetch_task: fetch::FetchTask,
 }
 
 pub enum Msg {
     Post(Post),
-    Submit(Event),
+    Submit(PostForm),
     Updated,
 }
 
@@ -40,7 +37,6 @@ impl Component for Model {
             link,
             props,
             post: None,
-            errors: Default::default(),
             fetch_task,
         }
     }
@@ -51,26 +47,13 @@ impl Component for Model {
                 self.post = Some(post);
                 true
             }
-            Msg::Submit(event) => {
-                event.prevent_default();
-                let form: PostForm = (&event).into();
-                web_sys::console::log_1(&format!("{:?}", &form).into());
-                match form.validate() {
-                    Err(errors) => {
-                        self.errors = errors;
-                        utils::scroll_to_error();
-                        true
-                    }
-                    Ok(()) => {
-                        self.errors = PostErrors::default();
-                        self.fetch_task = fetch::FetchService::new().put(
-                            &format!("/api/admin/posts/{}", self.props.id),
-                            &form,
-                            self.link.callback(|_: ()| Msg::Updated),
-                        );
-                        true
-                    }
-                }
+            Msg::Submit(form) => {
+                self.fetch_task = fetch::FetchService::new().put(
+                    &format!("/api/admin/posts/{}", self.props.id),
+                    &form,
+                    self.link.callback(|_: ()| Msg::Updated),
+                );
+                false
             }
             Msg::Updated => {
                 utils::toast("更新しました。");
@@ -81,40 +64,13 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
-        match self.post {
+        let callback = self.link.callback(|form: PostForm| Msg::Submit(form));
+        match &self.post {
             None => html! { "" },
-            Some(ref post) => html! {
+            Some(post) => html! {
               <>
                 <h1>{"編集"}</h1>
-                <form class="std" onsubmit=self.link.callback(|event| Msg::Submit(event))>
-                  <div>
-                    <label>{"タイトル"}</label>
-                    <input type="text" name="title" value=&post.title />
-                    <error::Model message={&self.errors.title} />
-                  </div>
-                  <div>
-                    <label>{"本文"}</label>
-                    <textarea name="body">{&post.body}</textarea>
-                    <error::Model message={&self.errors.body} />
-                  </div>
-                  <div>
-                    <label>{"ステータス"}</label>
-                    <div>
-                      <label>
-                        <input type="radio" name="status" value=PostStatus::Draft
-                               checked={post.status == PostStatus::Draft} />
-                        {"下書き"}
-                      </label>
-                      <label>
-                        <input type="radio" name="status" value=PostStatus::Published
-                               checked={post.status == PostStatus::Published} />
-                        {"公開"}
-                      </label>
-                    </div>
-                    <error::Model message={&self.errors.status} />
-                  </div>
-                  <button>{"更新"}</button>
-                </form>
+                <form::Model button_label="更新", onsubmit=callback post=post />
               </>
             },
         }
