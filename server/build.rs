63bd5ec003ff2892214054354cdea1f59b18e935
Tahara Yoshinori <read.eval.print@gmail.com>
2020-07-17 01:41:23 +0900

    enum の対応が必要だった

--- server/build.rs
+++ server/build.rs
@@ -1,28 +1,24 @@
-//use std::io::Write;
+use anyhow::Result;
+use arysn::generator::config::Config;
+use arysn::generator::define_ar;
+use quote::format_ident;
 
-/*
-↓このワーニングを消すために
-warning: unused import: `common::types::*`
- --> src/schema.rs:3:9
-  |
-3 |     use crate::common::types::*;
-  |         ^^^^^^^^^^^^^^^^^^^^^^^
-  |
-  = note: `#[warn(unused_imports)]` on by default
-*/
-fn main() {
-    //    let allow = "    #[allow(unused_imports)]";
-    //    let schema = std::fs::read_to_string("src/schema.rs").unwrap();
-    //    for line in schema.lines() {
-    //        if line == allow {
-    //            return;
-    //        }
-    //    }
-    //    let mut writer = std::io::BufWriter::new(std::fs::File::create("src/schema.rs").unwrap());
-    //    for line in schema.lines() {
-    //        if line == "    use common::types::*;" {
-    //            writeln!(writer, "{}", allow).unwrap();
-    //        }
-    //        writeln!(writer, "{}", line).unwrap();
-    //    }
+fn main() -> Result<()> {
+    define_ar(&Config {
+        path: "src/generated/user.rs",
+        table_name: "users",
+        struct_name: format_ident!("{}", "User"),
+        has_many: vec![],
+        belongs_to: vec![],
+    })?;
+
+    define_ar(&Config {
+        path: "src/generated/post.rs",
+        table_name: "posts",
+        struct_name: format_ident!("{}", "Post"),
+        has_many: vec![],
+        belongs_to: vec![],
+    })?;
+
+    Ok(())
 }
