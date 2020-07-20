cfbf5e8be59e8c13995284a4c01aaabb4134a606
Tahara Yoshinori <read.eval.print@gmail.com>
2020-07-20 19:34:23 +0900

    専用パッケージ

--- client/src/generated/post.rs
+++ client/src/generated/post.rs
@@ -1,1 +1,35 @@
-../../../server/src/generated/post.rs
+#[cfg(target_arch = "x86_64")]
+use postgres_types::{FromSql, ToSql};
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[cfg_attr(
+    target_arch = "x86_64",
+    derive(FromSql, ToSql),
+    postgres(name = "post_status")
+)]
+pub enum PostStatu {
+    #[cfg_attr(target_arch = "x86_64", postgres(name = "draft"))]
+    Draft,
+    #[cfg_attr(target_arch = "x86_64", postgres(name = "published"))]
+    Published,
+}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub status: PostStatu,
+    pub published_at: Option<chrono::NaiveDateTime>,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct PostNew {
+    pub id: Option<i32>,
+    pub title: String,
+    pub body: String,
+    pub status: PostStatu,
+    pub published_at: Option<chrono::NaiveDateTime>,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+}
--- client/src/generated/user.rs
+++ client/src/generated/user.rs
@@ -1,1 +1,33 @@
-../../../server/src/generated/user.rs
+#[cfg(target_arch = "x86_64")]
+use postgres_types::{FromSql, ToSql};
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[cfg_attr(
+    target_arch = "x86_64",
+    derive(FromSql, ToSql),
+    postgres(name = "user_status")
+)]
+pub enum UserStatu {
+    #[cfg_attr(target_arch = "x86_64", postgres(name = "active"))]
+    Active,
+    #[cfg_attr(target_arch = "x86_64", postgres(name = "locked"))]
+    Locked,
+}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub name: String,
+    pub status: UserStatu,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct UserNew {
+    pub id: Option<i32>,
+    pub email: String,
+    pub name: String,
+    pub status: UserStatu,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+}
